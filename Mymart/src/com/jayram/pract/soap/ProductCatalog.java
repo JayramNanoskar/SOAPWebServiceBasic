99cf9860cf9d5ccf1cd78c5d4337e6e11b8174e6
JayramNanoskar <jayram.n978@gmail.com>
2021-06-27 02:05:10 +0530

    Extracting SEI out of implementation

--- Mymart/src/com/jayram/pract/soap/ProductCatalog.java
+++ Mymart/src/com/jayram/pract/soap/ProductCatalog.java
@@ -2,33 +2,32 @@ package com.jayram.pract.soap;
 
 import java.util.List;
 
-import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 import com.jayram.pract.soap.model.Product;
 import com.jayram.pract.soap.service.ProductServiceImpl;
 
-@WebService(name="MyMartCatalog", portName="MyMartCatalogPort", serviceName="MyMartCatalogService", targetNamespace="http://www.testmart.com")
-public class ProductCatalog {
+@WebService(endpointInterface="com.jayram.pract.soap.ProductCatalogInterface", name="MyMartCatalog", portName="MyMartCatalogPort", serviceName="MyMartCatalogService")
+public class ProductCatalog implements ProductCatalogInterface {
 	
 	ProductServiceImpl productService = new ProductServiceImpl();
 	
-	@WebMethod(action="fetch_categories", operationName="fetchCategories")
+	@Override
 	public List<String> getProductCategories(){
 		return productService.getProductCategories();
 	}
 	
-	@WebMethod
+	@Override
 	public List<String> getProducts(String category){
 		return productService.getProducts(category);
 	}
 	
-	@WebMethod
+	@Override
 	public List<Product> getProductsv2(String category){
 		return productService.getProductsv2(category);
 	}
 
-	@WebMethod
+	@Override
 	public boolean addProduct(String category, String product){
 		return productService.addProduct(category, product);
 	}
---
+++ Mymart/src/com/jayram/pract/soap/ProductCatalogInterface.java
@@ -0,0 +1,25 @@
+package com.jayram.pract.soap;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import com.jayram.pract.soap.model.Product;
+
+@WebService(targetNamespace="http://www.testmart.com")
+public interface ProductCatalogInterface {
+	
+	@WebMethod(action="fetch_categories", operationName="fetchCategories")
+	public abstract List<String> getProductCategories();
+	
+	@WebMethod
+	public abstract List<String> getProducts(String category);
+	
+	@WebMethod
+	public abstract List<Product> getProductsv2(String category);
+	
+	@WebMethod
+	public abstract boolean addProduct(String category, String product);
+
+}
