2e8575bc645f0b966650d2544f683f94f0f3c6c1
JayramNanoskar <jayram.n978@gmail.com>
2021-06-26 16:06:28 +0530

    Writing a web service client: Calling the Webservice

--- NumberConversion/src/com/jayram/NumConversion.java
+++ NumberConversion/src/com/jayram/NumConversion.java
@@ -1,9 +1,23 @@
 package com.jayram;
 
+import java.math.BigInteger;
+
+import com.dataaccess.webservicesserver.NumberConversion;
+import com.dataaccess.webservicesserver.NumberConversionSoapType;
+
 public class NumConversion {
 
 	public static void main(String[] args) {
-		
+		if(args.length != 1){
+			System.out.println("Please enter the number");
+		}
+		else{
+			String number = args[0];
+			NumberConversion numberConversionService = new NumberConversion(); //Creating instance of service using Service name in WSDL
+			NumberConversionSoapType numberConversionSoap = numberConversionService.getNumberConversionSoap(); //Creating SOAP object using Port name in WSDL
+			String numberToWords = numberConversionSoap.numberToWords(BigInteger.valueOf(Integer.parseInt(number))); //Calling Web service
+			System.out.println(numberToWords);
+		}
 	}
 
 }
